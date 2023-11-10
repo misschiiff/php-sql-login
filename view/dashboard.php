5b805e569c2ee17171a772131c053d5ead079a11
BRIDGET IZBA <100096519+misschiiff@users.noreply.github.com>
2023-11-11 00:49:49 +0100

    Update dashboard.php

--- view/dashboard.php
+++ view/dashboard.php
@@ -14,6 +14,12 @@ if (! empty($_SESSION["userId"])) {
     }
 }
 ?>
+	<?php 
+session_start();
+$_SESSION["user_id"] = "";
+session_destroy();
+header("Location: index.php");
+
 <html>
 <head>
 <title>User Login</title>
